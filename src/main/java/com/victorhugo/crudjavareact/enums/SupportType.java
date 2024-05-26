19ce763ab41ac06355bfe3aaf16eb2afbaf69818
Victor <vhduartelima@hotmail.com>
2024-05-25 23:11:17 -0300

    feat: SupportInf and SupportType added to exercise use of Embeddable annotation

---
+++ src/main/java/com/victorhugo/crudjavareact/enums/SupportType.java
@@ -0,0 +1,7 @@
+package com.victorhugo.crudjavareact.enums;
+
+public enum SupportType {
+    HIGH_PRIORITY,
+    MEDIUM_PRIORITY,
+    LOW_PRIORITY
+}
---
+++ src/main/java/com/victorhugo/crudjavareact/model/SupportInf.java
@@ -0,0 +1,14 @@
+package com.victorhugo.crudjavareact.model;
+
+import com.victorhugo.crudjavareact.enums.SupportType;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Embeddable
+public class SupportInf {
+    private Long supportId;
+    private SupportType supportType;
+}

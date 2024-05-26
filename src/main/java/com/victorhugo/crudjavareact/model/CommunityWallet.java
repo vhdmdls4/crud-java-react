2cf77472edbca73ff27b89cf222a0fd2753d3e22
Victor <vhduartelima@hotmail.com>
2024-05-25 23:08:16 -0300

    feat: adding CommunityWallet to exercise ManyToMany

---
+++ src/main/java/com/victorhugo/crudjavareact/model/CommunityWallet.java
@@ -0,0 +1,31 @@
+package com.victorhugo.crudjavareact.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CommunityWallet{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+    private BigDecimal balance;
+    private BigDecimal debt;
+    private BigDecimal credit;
+    private BigDecimal availableBalance;
+
+    @ManyToMany(mappedBy = "communityWalletList")
+    private List<User> userList;
+    @OneToMany
+    private List<FinancialTransaction> financialTransactionList;
+
+    private SupportInf supportInf;
+}

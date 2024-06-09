bc699959c7515046aaf7f4b8101b86508e843f1c
Victor <vhduartelima@hotmail.com>
2024-06-09 16:08:36 -0300

    update: added content to WalletServices

--- src/main/java/com/victorhugo/crudjavareact/services/WalletServices.java
+++ src/main/java/com/victorhugo/crudjavareact/services/WalletServices.java
@@ -1,7 +1,10 @@
 package com.victorhugo.crudjavareact.services;
 
+import com.victorhugo.crudjavareact.DTO.CreateWalletDTO;
+import com.victorhugo.crudjavareact.DTO.WalletDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface WalletServices {
+    WalletDTO createWallet(CreateWalletDTO createWalletDTO);
 }
--- src/main/java/com/victorhugo/crudjavareact/services/impl/WalletServicesImpl.java
+++ src/main/java/com/victorhugo/crudjavareact/services/impl/WalletServicesImpl.java
@@ -1,8 +1,24 @@
 package com.victorhugo.crudjavareact.services.impl;
 
+import com.victorhugo.crudjavareact.DTO.CreateWalletDTO;
+import com.victorhugo.crudjavareact.DTO.WalletDTO;
+import com.victorhugo.crudjavareact.mapper.WalletMapper;
+import com.victorhugo.crudjavareact.model.Wallet;
+import com.victorhugo.crudjavareact.repository.WalletRepository;
+import com.victorhugo.crudjavareact.services.UserServices;
 import com.victorhugo.crudjavareact.services.WalletServices;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class WalletServicesImpl implements WalletServices {
+    final WalletRepository walletRepository;
+    final UserServices userServices;
+    @Override
+    public WalletDTO createWallet(CreateWalletDTO createWalletDTO) {
+        userServices.checkIfUserExists(createWalletDTO.getUser().getUsername(), createWalletDTO.getUser().getEmail());
+        Wallet createdWallet = walletRepository.save(WalletMapper.createWalletDTOtoEntity(createWalletDTO));
+        return WalletMapper.toDTO(createdWallet);
+    }
 }

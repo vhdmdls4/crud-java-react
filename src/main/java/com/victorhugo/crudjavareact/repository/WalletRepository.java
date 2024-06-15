eff1499f1ba5351dcc1bfaa5b8d4e72df9f607da
Victor <vhduartelima@hotmail.com>
2024-06-14 22:58:03 -0300

    update: added findByUser_Id to WalletRepository

--- src/main/java/com/victorhugo/crudjavareact/repository/WalletRepository.java
+++ src/main/java/com/victorhugo/crudjavareact/repository/WalletRepository.java
@@ -2,8 +2,12 @@ package com.victorhugo.crudjavareact.repository;
 
 import com.victorhugo.crudjavareact.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
+    Optional<Wallet> findByUser_Id(Long id);
 }

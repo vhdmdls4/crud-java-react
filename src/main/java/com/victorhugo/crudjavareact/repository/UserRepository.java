127a164dc17c29de8e55fca149779a43c1d74faf
Victor <vhduartelima@hotmail.com>
2024-08-19 23:24:10 -0300

    feat: added SecurityConfig

--- src/main/java/com/victorhugo/crudjavareact/repository/UserRepository.java
+++ src/main/java/com/victorhugo/crudjavareact/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.victorhugo.crudjavareact.repository;
 import com.victorhugo.crudjavareact.DTO.UserDTO;
 import com.victorhugo.crudjavareact.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import javax.validation.constraints.NotBlank;
@@ -13,6 +14,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsernameOrEmail(String username, String email);
+    UserDetails findByUsername(String username);
 
     Optional<Object> findByIdOrUsernameOrEmail(Long id, @NotBlank @NotEmpty String username, @NotBlank @NotEmpty String email);
     //TODO - filter the User Entity by deleted column
---
+++ src/main/java/com/victorhugo/crudjavareact/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.victorhugo.crudjavareact.security;
+
+import com.victorhugo.crudjavareact.exception.GenericApplicationException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagementConfigurer -> sessionManagementConfigurer
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
+    }
+}

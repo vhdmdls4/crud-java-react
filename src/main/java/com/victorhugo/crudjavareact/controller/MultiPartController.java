e16b18b0aa4ad9f99a50e423ed4df45e1b78f38e
Victor <vhduartelima@hotmail.com>
2024-08-24 01:36:57 -0300

    knowledge: now I know how to handle multipartFile inside a dto that has a list attribute remember to use: listaFiles.forEach((file, index) => {       formData.append(`listaFilhos[${index}].multipartFile`, file);       formData.append(`listaFilhos[${index}].name`, file.name);     });     const config = {           headers: {             "Content-Type": "multipart/form-data",           },         };

--- src/main/java/com/victorhugo/crudjavareact/controller/MultiPartController.java
+++ src/main/java/com/victorhugo/crudjavareact/controller/MultiPartController.java
@@ -1,15 +1,33 @@
 package com.victorhugo.crudjavareact.controller;
 
+import com.victorhugo.crudjavareact.DTO.MultiPartDTOChild;
 import com.victorhugo.crudjavareact.DTO.MultiPartDTOFather;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/test-multi")
 public class MultiPartController {
     @PostMapping
-    public ResponseEntity<?> handleMultiPartRequest(@RequestBody MultiPartDTOFather listaDTORecebido){
-        return ResponseEntity.ok("Ok");
+    @CrossOrigin(origins = "http://localhost:5173")
+    public ResponseEntity<?> handleMultiPartRequest(@ModelAttribute MultiPartDTOFather listaDTORecebido){
+        listaDTORecebido.getListaFilhos().forEach(child -> {
+            MultipartFile file = child.getMultipartFile();
+            try {
+                String content = new BufferedReader(new InputStreamReader(file.getInputStream()))
+                        .lines().collect(Collectors.joining("\n"));
+                System.out.println("Nome do arquivo: " + file.getOriginalFilename());
+                System.out.println("Conteúdo do arquivo:\n" + content);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        return ResponseEntity.ok("Arquivos processados com sucesso");
     }
 
     @GetMapping

ad30d1caac56010e15e76db2d434e38c80a964aa
Victor <vhduartelima@hotmail.com>
2024-09-01 23:13:53 -0300

    update: added exception with error cause

--- src/main/java/com/victorhugo/crudjavareact/exception/GenericApplicationException.java
+++ src/main/java/com/victorhugo/crudjavareact/exception/GenericApplicationException.java
@@ -4,4 +4,7 @@ public class GenericApplicationException extends RuntimeException{
     public GenericApplicationException(String errorMessage){
         super(errorMessage);
     }
+    public GenericApplicationException(String errorMessage, Throwable errorCause){
+        super(errorMessage, errorCause);
+    }
 }
